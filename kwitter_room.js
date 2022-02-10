16b74bbb0a883cca912b2e58c3a8393d69076b14
stutisinha29 <88232935+stutisinha29@users.noreply.github.com>
2022-02-10 19:23:57 +0530

    Add files via upload

---
+++ kwitter_room.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyCfrOGYmF57qKXRd6dbNOnBbGVpMVjvPz8",
+    authDomain: "kwitter-f9a3f.firebaseapp.com",
+    projectId: "kwitter-f9a3f",
+    storageBucket: "kwitter-f9a3f.appspot.com",
+    messagingSenderId: "847195421142",
+    appId: "1:847195421142:web:208a33e6331b8d3de40c77"
+  };
+
+  firebase.initializeApp(firebaseConfig);
